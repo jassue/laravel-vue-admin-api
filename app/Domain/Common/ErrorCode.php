a42265fdf11a579b6d70abd5b91f7948e4e3f6b7
jassue <jassue@163.com>
2020-04-06 13:55:02 +0000

    优化异常处理

--- app/Domain/Common/ErrorCode.php
+++ app/Domain/Common/ErrorCode.php
@@ -9,8 +9,23 @@ class ErrorCode
     const UNAUTHORIZED = 40100;
     const FORBIDDEN = 40300;
     const NOT_FOUND = 40400;
+    const MODEL_NOT_FOUND = 40401;
+    const HTTP_NOT_FOUND = 40402;
     const METHOD_NOT_ALLOWED = 40500;
     const UNPROCESSABLE_ENTITY = 42200;
     const INTERNAL_SERVER_ERROR = 50000;
     const SQL_ERROR = 60000;
+
+    const ErrorMsg = [
+        self::DEFAULT => 'Default error.',
+        self::UNAUTHORIZED => 'Unauthenticated.',
+        self::FORBIDDEN => 'This action is unauthorized.',
+        self::NOT_FOUND => 'Not Found',
+        self::MODEL_NOT_FOUND => 'Model Not Found',
+        self::HTTP_NOT_FOUND => 'HTTP Not Found',
+        self::METHOD_NOT_ALLOWED => 'Method Not Allowed',
+        self::UNPROCESSABLE_ENTITY => 'Unprocessable Entity',
+        self::INTERNAL_SERVER_ERROR => 'Internal Server Error',
+        self::SQL_ERROR => 'Query error.',
+    ];
 }
